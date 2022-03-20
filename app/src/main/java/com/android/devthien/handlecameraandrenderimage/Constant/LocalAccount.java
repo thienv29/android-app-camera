dd5a8ad10719c47343dd6f356dfc92aa88e22e7e
thienv29 <thienv29@gmail.com>
2022-03-21 00:10:35 +0700

    UPDATE LOGIN

---
+++ app/src/main/java/com/android/devthien/handlecameraandrenderimage/Constant/LocalAccount.java
@@ -0,0 +1,6 @@
+package com.android.devthien.handlecameraandrenderimage.Constant;
+
+public class LocalAccount {
+    public static final String LOCAL_USERNAME = "thien";
+    public static final String LOCAL_PASSWORD = "123";
+}
--- app/src/main/java/com/android/devthien/handlecameraandrenderimage/MainActivity.java
+++ app/src/main/java/com/android/devthien/handlecameraandrenderimage/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -17,6 +20,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.devthien.handlecameraandrenderimage.Constant.LocalAccount;
 import com.android.devthien.handlecameraandrenderimage.GridViewImage.GridViewImage;
 
 import java.util.ArrayList;
@@ -26,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
     public static final int CAMERA_PERM_CODE = 101;
     GridView imageView;
     Button takePhoto;
+    EditText usernameTx;
+    EditText passwordTx;
+    String username;
+    String password;
+    Button signIn;
     List<Bitmap> images = new ArrayList<>();
 
     @Override
@@ -35,12 +44,29 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.grid_view);
         takePhoto = findViewById(R.id.bottomButton);
-
+        signIn = findViewById(R.id.sign_in_btn);
+        usernameTx = findViewById(R.id.username);
+        passwordTx = findViewById(R.id.password);
 
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openCamera();
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!usernameTx.getText().toString().equals(LocalAccount.LOCAL_USERNAME) || !passwordTx.getText().toString().equals(LocalAccount.LOCAL_PASSWORD) ){
+                    Toast.makeText(MainActivity.this, "Tài khoản hoặc mật khẩu sai", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (usernameTx.getText().toString().equals(LocalAccount.LOCAL_USERNAME) && passwordTx.getText().toString().equals(LocalAccount.LOCAL_PASSWORD) ){
+                   username = usernameTx.getText().toString();
+                   password = passwordTx.getText().toString();
+                   Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_LONG).show();
+
+                }
             }
         });
 
@@ -75,7 +101,16 @@ public class MainActivity extends AppCompatActivity {
                     });
 
     private void openCamera() {
-        launchCamera.launch(Manifest.permission.CAMERA);
+        if (checkLogin()){
+            launchCamera.launch(Manifest.permission.CAMERA);
+        }
+    }
+    private boolean checkLogin(){
+        if (username == null || password == null || username.isEmpty() || password.isEmpty()){
+            Toast.makeText(MainActivity.this, "Bạn chưa đăng nhập !!!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 
 
